9bb6d65128ac311e5ebe5dc5d1c863f0309c0bc9
Aurelie D <adelaunay@palo-it.com>
2022-06-06 22:06:53 +0200

    Add release

--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,7 @@ import { BrowserTracing } from '@sentry/tracing';
 Sentry.init({
   dsn: 'https://7077cddcb5f241399789af4b2c7409ea@o1277637.ingest.sentry.io/6475161',
   integrations: [new BrowserTracing()],
+  release: "my-test-sentry@1.1.",
 
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for performance monitoring.
